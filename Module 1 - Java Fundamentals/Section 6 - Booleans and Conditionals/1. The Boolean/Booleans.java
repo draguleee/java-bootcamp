ca8abd184acb1f4b77bd21ca6fb989d145b8ce21
Andre Dragu <aandreid14@gmail.com>
2023-09-08 00:25:54 +0300

    temp

---
+++ Module 1 - Java Fundamentals/Section 6 - Booleans and Conditionals/1. The Boolean/Booleans.java
@@ -0,0 +1,24 @@
+/**
+ * Booleans
+ * 
+ * 1. 
+ */
+
+public class Booleans {
+    public static void main(String[] args) {
+        
+        // Define two boolean variables
+        boolean bool1 = true;
+        boolean bool2 = false;
+
+        // Print the booleans (for the sake of the exercise)
+        System.out.println(bool1);
+        System.out.println(bool2);
+    }
+    
+}
+
+/*
+ * - boolean take 1 bit of memory.
+ * - boolean can only have true / false values.
+ */
