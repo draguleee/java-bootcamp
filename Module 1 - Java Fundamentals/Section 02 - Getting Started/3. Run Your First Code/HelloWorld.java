ccf1d1a6483c375fb67c715ce862850df02288d0
Andre Dragu <aandreid14@gmail.com>
2023-09-25 11:47:17 +0300

    Getting Started

---
+++ Module 1 - Java Fundamentals/Section 02 - Getting Started/3. Run Your First Code/HelloWorld.java
@@ -0,0 +1,47 @@
+/**
+ * First Java code
+ * 
+ * The process:
+ * 
+ * 1. Create a folder.
+ * 
+ * 2. Launch the folder from Visual Studio Code.
+ * 
+ * 3. Create a Java file:
+ *    - Name your file CamelCase: first letter of every word is capitalized and the words are joined together.
+ *    - For the file to be a Java file, it has to have the .java extension.
+ *
+ * 4. Create a Java class insinde of the Java file:
+ *    - You must write all of the Java code in a class
+ *      * 'class': keyword
+ *      * 'HelloJava': class name
+ *      * {...} : code
+ *    - The class name needs to always match the file name
+ * 
+ * 5. Your class needsa main() method:
+ *    - main() is the entry point of a Java application
+ *    - This is where Java starts running your code
+ * 
+ * 6. Semi-colon: means end of statement and it is mandatory!
+ * 
+ * 7. Code is case-sensitive!
+ * 
+ * 8. Commands:
+ *    - Compile code: javac your_file.java
+ *    - Run code: java your_file
+ * 
+ * 9. Turn on autosave: File -> Autosave
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        /*
+         * println() is a function that prints a message to the console.
+         * In the parantheses, print your message.
+         * Semi-colon ( ; ) means end of the statement.
+         */
+        System.out.println("Hello, world!");
+
+    }
+}
