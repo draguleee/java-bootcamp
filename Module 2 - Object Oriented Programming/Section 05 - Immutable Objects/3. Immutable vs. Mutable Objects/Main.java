ddb9d163016e4e78207c137b578f0c549e5ed2f6
draguleee <aandreid14@gmail.com>
2024-03-11 20:57:47 +0200

    Immutable Objects

---
+++ Module 2 - Object Oriented Programming/Section 05 - Immutable Objects/3. Immutable vs. Mutable Objects/Main.java
@@ -0,0 +1,33 @@
+/**
+ * Main Class
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+        // **************** Immutable Object ****************
+        
+        Integer applesWrapper = 5;
+        Integer applesWrapper2 = applesWrapper;     // SAFE
+        applesWrapper2 = 10;
+
+        System.out.println(applesWrapper);
+        System.out.println(applesWrapper2);
+
+        String text = "hi";
+        text = "hello";
+
+
+        // **************** Mutable Object ****************
+        
+        City city = new City("Paris", 2161000);
+        // City secondCity = city;            // DANGEROUS
+        City secondCity = new City(city);
+
+        city.setPopulation(2261000);
+        secondCity.setPopulation(2263400);
+        city.setPopulation(2163400);
+        secondCity.setPopulation(2443400);
+        
+    }
+}
